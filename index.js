bb58d2bbfb640a6397f74233fb63392335b7f6d3
iamchacha021 <iamchacha021@gmail.com>
2022-12-06 08:45:51 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // code your solution here
+const record = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+  ]
+
+  function superbowlWin(stat){
+    const yearWithWin = stat.find(function(record){
+        return record.result=== 'W'
+    })
+    return yearWithWin === undefined? undefined : yearWithWin.year;
+  }
